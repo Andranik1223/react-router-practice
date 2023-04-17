6808caa4069d1866fb37e4ce19f91ebaa17f6ddb
Andranik1223 <sargsyan.andranik97@gmail.com>
2023-04-17 08:20:31 -0800

    setup router navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Link to="/contact">Contact</Link>
     </div>
   );
 }
