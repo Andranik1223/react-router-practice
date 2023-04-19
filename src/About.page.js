8dd1be2d3344c1819f43ac957e8a7449a08cab5c
Andranik1223 <sargsyan.andranik97@gmail.com>
2023-04-19 11:04:37 -0800

    added params

--- src/About.page.js
+++ src/About.page.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const About = () => {
+  const { id } = useParams();
+
   return (
     <div>
-       <h1>About</h1> 
-       <Link to="/">Home Page</Link>
-       <Link to="/">Contact Page</Link>
+      <h1>About</h1>
+      <h2>{id}</h2>
+      <Link to="/">Home Page</Link>
+      <Link to="/">Contact Page</Link>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
